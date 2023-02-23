ad7293fd7bc275ff33741c03eac530022a68de2a
Naveen Polasa <naveenpolasa99@gmail.com>
2023-02-24 00:24:05 +0530

    update code files

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
 const About = () => {
   return (
-    <div className="w-[66%] mx-auto">
+    <div className="max-w-7xl mx-auto">
+      <div className="w-[66%] mx-auto">
         <h3 className="text-center text-3xl my-5 font-semibold">About Us</h3>
-        <p className="px-6 py-5 text-lg opacity-80">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum debitis quasi officia veniam magnam ratione molestias eum? Temporibus veritatis non, dicta accusantium deserunt voluptas facere dolore aliquam, nam iusto amet laboriosam quod! Deleniti incidunt minus similique ab non nam possimus consequatur laudantium voluptate fugiat soluta, nulla eum. Ea quis maiores illo saepe, at aspernatur eaque.</p>
+        <p className="px-6 py-5 text-lg opacity-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum debitis
+          quasi officia veniam magnam ratione molestias eum? Temporibus
+          veritatis non, dicta accusantium deserunt voluptas facere dolore
+          aliquam, nam iusto amet laboriosam quod! Deleniti incidunt minus
+          similique ab non nam possimus consequatur laudantium voluptate fugiat
+          soluta, nulla eum. Ea quis maiores illo saepe, at aspernatur eaque.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 export default About
